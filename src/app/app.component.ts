af8cdcfb3d4f65f557b72dde2888c7fc7f463a9b
Necdet UYGUR <necdet.uygur@gmail.com>
2023-03-04 17:30:42 +0300

    🚀

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { GeneratorService } from './services/generator.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password-generator';
+  constructor(private generatorService: GeneratorService) {}
+  parola: string = '';
+  uzunluk: number = 6;
+  tips = {
+    alfabe: false,
+    sayi: false,
+    sembol: false,
+  };
+
+  uzunlukChange(e: any) {
+    this.uzunluk = e.target.value * 1;
+  }
+
+  chChange(e: any) {
+    (this.tips as any)[e.target.value] = e.target.checked;
+  }
+
+  olusturBtnClick() {
+    this.parola = this.generatorService.Generate(
+      this.uzunluk,
+      this.tips.alfabe,
+      this.tips.sayi,
+      this.tips.sembol
+    );
+  }
 }
